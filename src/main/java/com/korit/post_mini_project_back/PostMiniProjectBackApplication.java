108a9fcf2a48be07bbecd7c8d8b26b4198aaae05
smithrowe-10 <smj0418smj@gmail.com>
2025-12-15 14:26:24 +0900

    first commit

---
+++ src/main/java/com/korit/post_mini_project_back/PostMiniProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.korit.post_mini_project_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMiniProjectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostMiniProjectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korit/post_mini_project_back/PostMiniProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.post_mini_project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMiniProjectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
